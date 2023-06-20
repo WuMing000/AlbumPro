1d9f6a7b2fcc3c4af7a437f671844066d712e935
WuQiLin001 <1715049618@qq.com>
2023-06-20 10:05:53 +0800

    修改域名和应用名称

--- app/src/main/java/com/js/photoalbum/manager/Contact.java
+++ app/src/main/java/com/js/photoalbum/manager/Contact.java
@@ -2,10 +2,10 @@ package com.js.photoalbum.manager;
 
 public class Contact {
 
-    public static final String SERVER_URL = "http://fs.arzopa.cn";
+    public static final String SERVER_URL = "http://fs.gworldtek.com";
     public static final String SERVER_PORT = "8086";
 
-    public static final String FTP_SERVER_IP = "fs.arzopa.cn";
+    public static final String FTP_SERVER_IP = "fs.gworldtek.com";
     public static final int FTP_SERVER_PORT = 21;
     public static final String FTP_SERVER_USERNAME = "ftp_usera";
     public static final String FTP_SERVER_PASSWORD = "123456";
